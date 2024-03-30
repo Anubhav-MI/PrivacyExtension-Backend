c2b8856348db0dbdb5bbfe603c0cf781f6354491
Anubhav-MI <120900218+Anubhav-MI@users.noreply.github.com>
2024-03-30 19:45:43 +0530

    backend updated

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-import { express } from "express";
+import express from "express";
+import { spawn } from "child_process";
+import cors from "cors";
 
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.post("/scrapper", (req, res) => {});
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.post("/scrapper", (req, res) => {
+  const { url } = req.body;
+  const pythonProcess = spawn("python", ["scraper.py", url]);
+
+  // Collect data from stdout
+  let scrapedTerms = "";
+  pythonProcess.stdout.on("data", (data) => {
+    scrapedTerms += data.toString();
+  });
+
+  // Handle process completion
+  pythonProcess.on("close", (code) => {
+    if (code === 0) {
+      res.status(200).send(scrapedTerms);
+    } else {
+      res.status(500).send("Error scraping terms and conditions.");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
