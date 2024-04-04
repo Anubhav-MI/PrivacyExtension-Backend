97e196dea888039f2067ab15a61b674e2f9a894a
Anubhav-MI <120900218+Anubhav-MI@users.noreply.github.com>
2024-04-04 23:11:30 +0530

    gemini api integrated

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 import express from "express";
-import { spawn } from "child_process";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
 import cors from "cors";
+dotenv.config();
 
 const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.post("/scrapper", (req, res) => {
-  const { url } = req.body;
+// console.log(process.env.GOOGLE_API_KEY);
 
-  // Collect data from stdout
+const genAI = new GoogleGenerativeAI(`${process.env.GOOGLE_API_KEY}`);
+
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+// const generationConfig = { temperature: 0.5, maxTokens: 100 };
+
+app.post("/summarize", async (req, res) => {
+  const { text } = req.body;
+
+  try {
+    const prompt = `Give the summarized points from the following terms and conditions and privacy policy. In short points 10 words each. ${text}`;
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const generatedText = await response.text();
+    console.log(generatedText);
+
+    res.status(200).send(generatedText);
+  } catch (error) {
+    console.error("Error generating text:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => {
